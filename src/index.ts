33054663bbece353ae4f31814576c4a4391f79a5
Petter Andersson <petter.andersson@helsingborg.se>
2022-11-10 08:50:46 +0100

    :poop: rename properties

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ const services: Services = createServicesFromEnv()
 
 /** Start daemon */
 services.smsListenerService.listen(async message => await services.smsSendService.send({
-	to: message.number, 
-	content: await services.smsContentService.build(message),
+	receiver: message.number,
+	message: await services.smsContentService.build(message),
 })).catch(err => {
 	console.error(err)
 	process.exit(1)
